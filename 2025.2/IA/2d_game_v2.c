8a7c163dc20f18501620467b2e3c72465bc7b7fd
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2025-09-18 01:55:32 -0300

    adicionas funções: limparTelaANSI, mover_robo; além de melhorar função imprime_lab

--- 2025.2/IA/2d_game_v2.c
+++ 2025.2/IA/2d_game_v2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define tam 5
 
-void imprime_lab(char labirinto[tam][tam], int linha, int coluna)
+void limparTelaANSI() 
+{
+    printf("\033[2J\033[H");
+}
+
+void imprime_lab(char labirinto[tam][tam], int linha_robo, int coluna_robo)
 {
     for(int i=0; i<tam; i++) //inicialização
     {
@@ -12,8 +18,8 @@ void imprime_lab(char labirinto[tam][tam], int linha, int coluna)
     }
 
     labirinto[1][1] = labirinto[2][1] = labirinto[4][1] = labirinto[0][3] = labirinto[1][3] = labirinto[3][3] = '#'; // posições paredes
-
-    labirinto[linha][coluna] = 'S'; // posição S
+    labirinto[3][4] = 'G'; // posição objetivo
+    labirinto[linha_robo][coluna_robo] = 'S'; // posição robo
 
     for(int i=0; i<tam; i++) //impressão
     {
@@ -25,19 +31,79 @@ void imprime_lab(char labirinto[tam][tam], int linha, int coluna)
     }
 }
 
-int posicao_robo(linha, coluna)
+void mover_robo(char movimento, int *linha_ptr, int *coluna_ptr)
 {
-    
+    switch(movimento)
+    {
+        case 'w':
+        case 'W':
+            (*linha_ptr)--;
+            if(*linha_ptr < 0)
+            {
+                *linha_ptr = 0;
+            }
+            break;
+        case 'a':
+        case 'A':
+            (*coluna_ptr)--;
+            if(*coluna_ptr < 0)
+            {
+                *coluna_ptr = 0;
+            }
+            break;
+        case 's':
+        case 'S':
+            (*linha_ptr)++;
+            if(*linha_ptr >= tam)
+            {
+                *linha_ptr = 4;
+            }
+            break;
+        case 'd':
+        case 'D':
+            (*coluna_ptr)++;
+            if(*coluna_ptr >= tam)
+            {
+                *coluna_ptr = 4;
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 int main()
 {
-    char labirinto[tam][tam]; // aloca a matriz
-    int linha = 0, coluna = 0;
-    
-    imprime_lab(labirinto, linha, coluna);
+    char labirinto[tam][tam], movimento;
+    int linha = 0, coluna = 0; 
 
-    // scanf(" %c", &movimento);
+    limparTelaANSI();
     
+    printf("Movimente [S] para chegar em [G]\n\tMova usando WASD\n\n");
+
+    while(1)
+    {
+        imprime_lab(labirinto, linha, coluna);
+
+        if(labirinto[3][4] == 'S')
+        {
+            printf("\nParabens! Objetivo atingido!\n");
+            break;
+        }
+
+        printf("\nInsira o movimento: ");
+        scanf(" %c", &movimento);
+
+        if(movimento == 'Q' || movimento == 'q')
+        {
+            break;
+        }
+
+        mover_robo(movimento, &linha, &coluna);
+
+        limparTelaANSI();
+    }
+
+    printf("\tExecucao encerrada!\n");
     return 0;
-}
+}   
