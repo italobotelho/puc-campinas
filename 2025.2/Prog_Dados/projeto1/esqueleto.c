cc72de7591cc3ff6e0df6291d69673f3c1c6f712
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2025-09-29 21:45:20 -0300

    feat: comparacao de vetores / jogo está jogável

--- 2025.2/Prog_Dados/projeto1/esqueleto.c
+++ 2025.2/Prog_Dados/projeto1/esqueleto.c
@@ -7,7 +7,7 @@
 // Chame essa função para preencher um vetor numero de tamanho n com um número
 // aleatório entre 00000 e 99999
 void gera_numero(int numero[], int n) {
-  // srand(time(NULL));
+  srand(time(NULL));
 
   for (int i = 0; i < n; ++i)
     numero[i] = rand() % 10;
@@ -39,9 +39,8 @@ void imprime_acertados(int numero[], int acertados[], int n) {
 
 int main() {
     // ======== 1 e 2 - Inicialização e Configuração ========
-    // numero gerado = 17409
     int vetor[TAM], vetor_numero[TAM], vetor_bool[TAM];
-    int tentativas, numero, digito;
+    int tentativas, numero, digito, aux;
 
     gera_numero(vetor, TAM);
 
@@ -49,19 +48,19 @@ int main() {
 
     do
     {
-        printf("Digite o numero de tentativas para encontrar o numero: ");
-        scanf(" %d", &tentativas);
+      printf("Digite o numero de tentativas para encontrar o numero: ");
+      scanf(" %d", &tentativas);
     } while (tentativas <= 0);
 
     // ======== 3 - Loop do Jogo ========
 
-    for(int i=0; i<tentativas; i++)
+    for(int i = 0; i < tentativas; i++)
     {
       do
       {
         printf("Digite um numero: ");
         scanf(" %d", &numero);
-      } while (numero <= 0);
+      } while (numero < 0);
 
       if(numero == devolve_numero(vetor, TAM))
       {
@@ -69,22 +68,29 @@ int main() {
       }
       else
       {
-        for(int j=TAM; j>0; j--)
+        for (int a = 0; a < TAM; a++)
         {
-          if(numero == 0)
-          {
-            break;
-          }
-
-          digito = numero % 10;
-          numero /= 10; 
-    
-          vetor_numero[j] = digito;
+          vetor_bool[a] = 0;
+          vetor_numero[a] = 0;
         }
-        
-        // imprime_acertados(vetor, vetor_bool, TAM);
-      }
 
+        aux = numero;
+        for (int j = TAM - 1; j >= 0; j--)
+        {
+          vetor_numero[j] = aux % 10;
+          aux /= 10;
+        }
+
+        for (int k = 0; k < TAM; k++)
+        {
+          if (vetor[k] == vetor_numero[k])
+          {
+            vetor_bool[k] = 1;
+          }
+        }
+
+        imprime_acertados(vetor, vetor_bool, TAM);
+      }
     }
 
     // 4 - Finalização
