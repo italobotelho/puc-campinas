6ee70d13000f7300d19415f794b344ea715ad972
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2025-10-15 10:55:45 -0300

    estudando funcoes em C

--- 2025.2/Prog_Dados/listas/funcoes/4.c
+++ 2025.2/Prog_Dados/listas/funcoes/4.c
@@ -4,21 +4,49 @@
 float media_maior_menor(float a, float b, float c, float *media, float *pt_maior, float *pt_menor)
 {
     *media = (a + b + c) / 3;
+
+    float maior = a, menor = a;
+    
+    if(b > maior)
+    {
+        maior = b;
+    }
+    else if(c > maior)
+    {
+        maior = c;
+    }
+
+    if(b < menor)
+    {
+        menor = b;
+
+    }
+    else if(c < menor)
+    {
+        menor = c;
+    }
+
+    *pt_maior = maior;
+    *pt_menor = menor;
 }
 
 int main()
 {
     float n1, n2, n3, media, maior, menor;
 
-    printf("Insira um valor:");
+    system("cls");
+
+    printf("Insira um valor: ");
     scanf("%f", &n1);
-    printf("Insira um valor:");
+    printf("Insira um valor: ");
     scanf("%f", &n2);
-    printf("Insira um valor:");
+    printf("Insira um valor: ");
     scanf("%f", &n3);
 
+    system("cls");
+
     media_maior_menor(n1, n2, n3, &media, &maior, &menor);
-    printf("Média: %.2f\nMaior: %.2f\nMenor: %.2f", media, maior, menor);
+    printf("--- Resultados ---\n\nMedia: %.2f\nMaior: %.2f\nMenor: %.2f", media, maior, menor);
 
     return 0;
 }
