d1b7ae4eb52e99f243a58e7102a10dba70f37544
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2025-10-17 15:03:31 -0300

    Corrigindo bugs de caracteres (acentos)

--- 2025.2/IA/genetic_algorithm/project.c
+++ 2025.2/IA/genetic_algorithm/project.c
@@ -1,9 +1,9 @@
 /*
  * nqueens_ga_min.c
- * Estrutura geral de um Algoritmo GenC)tico para N-Rainhas (N=4), versC#o enxuta.
- * - SaC-da apenas no terminal (stdout).
- * - Sem escrita de mC)tricas/arquivos.
- * - FunC'C5es com TODOs para o aluno completar.
+ * Estrutura geral de um Algoritmo Genetico para N-Rainhas (N=4), versao enxuta.
+ * - Saida apenas no terminal (stdout).
+ * - Sem escrita de metricas/arquivos.
+ * - Funcoes com TODOs para o aluno completar.
  *
  * Compilar: gcc -Wall -O2 nqueens_ga_min.c -o nqueens
  * Executar : ./nqueens            (seed baseada no tempo)
@@ -15,22 +15,22 @@
 #include <time.h>
 #include <math.h>
 
-/* ---------------- ParC"metros do problema e do GA ---------------- */
+/* ---------------- Parametros do problema e do GA ---------------- */
 #define N               4           /* tamanho do tabuleiro */
-#define POP_SIZE        100         /* tamanho da populaC'C#o */
-#define MAX_GEN         1000        /* mC!x. geraC'C5es */
+#define POP_SIZE        100         /* tamanho da populacao */
+#define MAX_GEN         1000        /* max. geracoes */
 #define CROSS_RATE      0.80f       /* taxa de crossover */
-#define MUT_RATE        0.10f       /* taxa de mutaC'C#o por gene */
+#define MUT_RATE        0.10f       /* taxa de mutacao por gene */
 #define ELITE_COUNT     2           /* nB: de elites preservados */
 #define TOURNAMENT_K    3           /* tamanho do torneio */
 
-/* ---------------- RepresentaC'C#o de indivC-duo -------------------- */
+/* ---------------- Representacao de individuo -------------------- */
 typedef struct {
 	int gene[N];     /* gene[i] = coluna da rainha na linha i */
-	int fitness;     /* aptidC#o */
+	int fitness;     /* aptidao */
 } Individual;
 
-/* -------------------- Utilidades aleatC3rias --------------------- */
+/* -------------------- Utilidades aleatorias --------------------- */
 static inline int rand_int(int a, int b) {
 	/* inteiro uniforme em [a,b] (assume a <= b) */
 	return a + rand() % (b - a + 1);
@@ -41,7 +41,7 @@ static inline double rand_unit() {
 	return (double)rand() / (double)(RAND_MAX);
 }
 
-/* -------------------- ProtC3tipos das funC'C5es -------------------- */
+/* -------------------- Prototipos das funcoes -------------------- */
 void random_individual(Individual *ind);
 int  compute_conflicts(const Individual *ind);
 int  evaluate(Individual *ind);
@@ -53,10 +53,10 @@ void one_point_crossover(const Individual *p1, const Individual *p2,
 void mutate(Individual *ind);
 void print_board_ascii(const Individual *ind);
 
-/* -------------------- ImplementaC'C5es (STUBs) -------------------- */
+/* -------------------- Implementacoes (STUBs) -------------------- */
 void random_individual(Individual *ind) {
 	// TODO: para cada i em [0..N-1], sorteie ind->gene[i] em [0, N-1].
-	// Inicie ind->fitness = 0 (serC! definido em evaluate).
+	// Inicie ind->fitness = 0 (sera definido em evaluate).
 	for(int i=0; i<N; i++)
 	{
 		ind->gene[i] = rand_int(0, N-1);
@@ -77,12 +77,12 @@ int compute_conflicts(const Individual *ind) {
 	{
 		for(int j=0; j<N; j++)
 		{
-			if(ind->gene[i] == ind->gene[j] || abs(i-j) == abs(ind->gene[i] - ind->gene[j])
-		{
-			cont++;
+			if((ind->gene[i] == ind->gene[j]) || (abs(i-j) == abs(ind->gene[i] - ind->gene[j])))
+			{
+				cont++;
+			}
 		}
 	}
-}
 
 (void)ind;
 
@@ -96,8 +96,8 @@ int evaluate(Individual *ind) {
 	   ind->fitness = total_pairs - conflicts
 	   return ind->fitness; */
 
-    total_pairs = N*(N-1)/2;
-    conflicts   = compute_conflicts(ind);
+    int total_pairs = N*(N-1)/2;
+    int conflicts = compute_conflicts(ind);
     ind->fitness = total_pairs - conflicts;
     return ind->fitness;
 	   
@@ -110,7 +110,7 @@ int cmp_desc_fitness(const void *a, const void *b) {
 	const Individual *ib = (const Individual*)b;
 	(void)ia;
 	(void)ib;
-	return 0; /* provisC3rio */
+	return 0; /* provisorio */
 }
 
 void copy_individual(const Individual *src, Individual *dst) {
@@ -121,11 +121,11 @@ void copy_individual(const Individual *src, Individual *dst) {
 
 int tournament_select(Individual pop[], int pop_size) {
 	/* TODO:
-	   - sorteie TOURNAMENT_K C-ndices em [0, pop_size-1]
-	   - retorne o C-ndice do indivC-duo com maior fitness */
+	   - sorteie TOURNAMENT_K indices em [0, pop_size-1]
+	   - retorne o indice do individuo com maior fitness */
 	(void)pop;
 	(void)pop_size;
-	return 0; /* provisC3rio */
+	return 0; /* provisorio */
 }
 
 void one_point_crossover(const Individual *p1, const Individual *p2,
@@ -133,7 +133,7 @@ void one_point_crossover(const Individual *p1, const Individual *p2,
 	/* TODO:
 	   - Com prob. CROSS_RATE: escolher um corte em [1, N-1),
 	     combinar prefixo de p1 com sufixo de p2 (c1) e o inverso (c2)
-	   - Caso contrC!rio: apenas copiar p1->c1 e p2->c2 */
+	   - Caso contrario: apenas copiar p1->c1 e p2->c2 */
 	(void)p1;
 	(void)p2;
 	(void)c1;
@@ -168,13 +168,13 @@ int main(int argc, char *argv[]) {
 
 	Individual pop[POP_SIZE], new_pop[POP_SIZE];
 
-	/* 1) InicializaC'C#o e avaliaC'C#o */
+	/* 1) Inicializacao e avaliacao */
 	for (int i = 0; i < POP_SIZE; ++i) {
 		random_individual(&pop[i]);  /* TODO */
 		evaluate(&pop[i]);           /* TODO */
 	}
 
-	/* 2) OrdenaC'C#o inicial (facilita elitismo) */
+	/* 2) Ordenacao inicial (facilita elitismo) */
 	qsort(pop, POP_SIZE, sizeof(Individual), cmp_desc_fitness); /* TODO */
 
 	const int TOTAL_PAIRS = N * (N - 1) / 2;
@@ -188,7 +188,7 @@ int main(int argc, char *argv[]) {
 			copy_individual(&pop[e], &new_pop[e]); /* TODO */
 		}
 
-		/* 3b) ReproduC'C#o: seleC'C#o, crossover, mutaC'C#o */
+		/* 3b) Reproducao: selecao, crossover, mutacao */
 		for (int i = ELITE_COUNT; i < POP_SIZE; i += 2) {
 			int p1 = tournament_select(pop, POP_SIZE); /* TODO */
 			int p2 = tournament_select(pop, POP_SIZE); /* TODO */
@@ -204,29 +204,29 @@ int main(int argc, char *argv[]) {
 			if (i + 1 < POP_SIZE) new_pop[i + 1] = c2;
 		}
 
-		/* 3c) SubstituiC'C#o e ordenaC'C#o */
+		/* 3e Substituicao e ordenacao */
 		for (int i = 0; i < POP_SIZE; ++i) pop[i] = new_pop[i];
 		qsort(pop, POP_SIZE, sizeof(Individual), cmp_desc_fitness); /* TODO */
 
-		/* 3d) Parada por soluC'C#o C3tima (fitness = C(N,2)) */
+		/* 3d) Parada por solucao otima (fitness = C(N,2)) */
 		if (pop[0].fitness == TOTAL_PAIRS) {
 			best_gen_found = gen;
 			break;
 		}
 	}
 
-	/* 4) SaC-da no terminal: melhor indivC-duo e tabuleiro */
+	/* 4) Saida no terminal: melhor individuo e tabuleiro */
 	Individual best = pop[0];
-	printf("Melhor fitness: %d (C3timo = %d)\n", best.fitness, TOTAL_PAIRS);
+	printf("Melhor fitness: %d (Otimo = %d)\n", best.fitness, TOTAL_PAIRS);
 	printf("Cromossomo (lin->col): ");
 	for (int i = 0; i < N; ++i) printf("%d ", best.gene[i]);
 	printf("\n\nTabuleiro:\n");
 	print_board_ascii(&best);  /* TODO */
 
 	if (best_gen_found >= 0)
-		printf("\nSoluC'C#o C3tima alcanC'ada na geraC'C#o: %d\n", best_gen_found);
+		printf("\nSolucao Otima alcancada na geracao: %d\n", best_gen_found);
 	else
-		printf("\nSoluC'C#o C3tima NCO alcanC'ada (ajuste parC"metros/operadores).\n");
+		printf("\nSolucao Otima NAO alcancada (ajuste parametros/operadores).\n");
 
 	return 0;
 }
